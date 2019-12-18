967a4b534faad6a57ed26d4fee3a713d7170a9ec
Xusi Han <han273@purdue.edu>
2019-12-18 15:00:56 -0500

    Update parameter description

--- VESPER_code/chkcmdline.c
+++ VESPER_code/chkcmdline.c
@@ -130,7 +130,7 @@ int chkcmdline( int argc, char **argv,CMD *cmd){
 }
 
 void errmsg(){
-	puts("Usage: EMVEC_FIT -a [MAP1.mrc (large)] -b [MAP2.mrc (small)] [(option)]");
+	puts("Usage: VESPER -a [MAP1.mrc (large)] -b [MAP2.mrc (small)] [(option)]");
 	puts("v0.10	Start");
 	puts("v0.20	Pure Multi threading Process & refine 5 degree.");
 	puts("v0.30	Add Overlap Mode & using single-precision");
@@ -141,10 +141,10 @@ void errmsg(){
 	puts("---Options---");
 	printf("-t [float] : Threshold of density map1 def=%.3f\n",0.00);
 	printf("-T [float] : Threshold of density map2 def=%.3f\n",0.00);
-	printf("-g [float] : Bandwidth of the gaussian filter\n");
+	printf("-g [float] : Bandwidth of the Gaussian filter\n");
         printf("             def=16.0, sigma = 0.5*[float]\n");
-	printf("-s [float] : Sampling grid space def=7.0\n");
-	printf("-A [float] : Sampling Angle interval def=30.0\n");
+	printf("-s [float] : Sampling voxel spacing def=7.0\n");
+	printf("-A [float] : Sampling angle spacing def=30.0\n");
 	printf("-c [int  ] : Number of cores for threads def=%d\n",2);
 	printf("-N [int  ] : Refine Top [int] models def=10\n");
 	printf("-S 	   : Show topN models in PDB format def=false\n");
@@ -154,5 +154,5 @@ void errmsg(){
         printf("             Using normalized density value by Gaussian Filter\n");
 	printf("-P         : Pearson Correlation Coefficient Mode def=false\n");
         printf("             Using normalized density value by Gaussian Filter and average density\n");
-	printf("Thi is Ver %.3f\n",VER);
+	printf("This is Ver %.3f\n",VER);
 }
